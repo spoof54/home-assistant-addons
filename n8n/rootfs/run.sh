9b1375805a33230d03747854c376fe4772310914
spoof54 <62078618+spoof54@users.noreply.github.com>
2025-10-02 11:42:42 +0200

    Update run.sh

--- n8n/rootfs/run.sh
+++ n8n/rootfs/run.sh
@@ -7,19 +7,13 @@ get_config() {
   jq -r ".options.\"$key\"" /data/config.json
 }
 
-# Lire config.json
-HOST=$(get_config "host")
-PROTOCOL=$(get_config "protocol")
-PORT=$(get_config "port")
+# Lire les options configurées dans l'UI
 WEBHOOK_URL=$(get_config "webhook_url")
 BASE_API=$(get_config "base_api")
 BASIC_USER=$(get_config "basic_auth_user")
 BASIC_PASS=$(get_config "basic_auth_password")
 
 # Exporter comme variables d’environnement pour n8n
-export N8N_HOST="${HOST}"
-export N8N_PROTOCOL="${PROTOCOL}"
-export N8N_PORT="${PORT}"
 export WEBHOOK_URL="${WEBHOOK_URL}"
 export N8N_ENDPOINT_REST="${BASE_API}"
 
@@ -29,9 +23,6 @@ export N8N_BASIC_AUTH_USER="${BASIC_USER}"
 export N8N_BASIC_AUTH_PASSWORD="${BASIC_PASS}"
 
 echo "🚀 Lancement de n8n avec la configuration suivante :"
-echo "- Host: ${HOST}"
-echo "- Protocol: ${PROTOCOL}"
-echo "- Port: ${PORT}"
 echo "- Webhook URL: ${WEBHOOK_URL}"
 echo "- Base API: ${BASE_API}"
 echo "- Basic Auth User: ${BASIC_USER}"
